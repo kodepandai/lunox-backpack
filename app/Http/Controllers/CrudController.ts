efac663f3bc4ba836dc1cee996707ce4631fbf25
Akhmad Salafudin <axmad386@gmail.com>
2022-06-17 01:12:51 +0700

    feat(crud): added base CrudController

--- app/Http/Controllers/CrudController.ts
+++ app/Http/Controllers/CrudController.ts
@@ -1,9 +1,14 @@
 import CrudPanel from "app/Library/CrudPanel/CrudPanel";
-import { Controller, ObjectOf } from "lunox";
+import { Controller, ObjectOf, Str } from "lunox";
+import type { Middleware } from "lunox/dist/Contracts/Http/Middleware";
 import type { ListOperation } from "./Operations/ListOperation";
 
 // CrudController may have this methods from Crud Traits
 interface CrudController extends ListOperation {}
+
+export interface CrudContext {
+  crud: CrudPanel;
+}
 
 class CrudController extends Controller {
   public crud!: CrudPanel;
@@ -18,19 +23,78 @@ class CrudController extends Controller {
     this.middleware({
       handle: async (req, next) => {
         // we just need asign this.crud once
-
-        // TODO: for now crud cannot be singleton, http request will be conflicted
         this.crud = new CrudPanel();
 
+        // TODO: for now crud cannot be singleton, http request will be conflicted
         this.crud.setRequest(req);
+        this.setupDefaults();
         this.setup();
+        this.setupConfigurationForCurrentOperation();
         return next(req);
       },
     });
   }
 
+  /**
+   * Allow developers to set their configuration options for a CrudPanel.
+   */
   public setup() {
-    console.log("run setup");
+    // pass
+  }
+
+  /**
+   * Load routes for all operations.
+   * Allow developers to load extra routes by creating a method that looks like setupOperationNameRoutes.
+   */
+  public setupRoutes(segment: string, controller: typeof CrudController) {
+    const matchedMethods = get_class_methods(this)
+      .join(";")
+      .match(/(?<=^|;)setup([^;]+?)Routes(;|$)/g)
+      ?.map((x) => x.replace(";", ""));
+    matchedMethods?.forEach((method) => {
+      (this as any)[method](segment, controller);
+    });
+  }
+
+  /**
+   * Load defaults for all operations.
+   * Allow developers to insert default settings by creating a method
+   * that looks like setupOperationNameDefaults.
+   */
+  public setupDefaults() {
+    const matchedMethods = get_class_methods(this)
+      .join(";")
+      .match(/(?<=^|;)setup([^;]+?)Defaults(;|$)/g)
+      ?.map((x) => x.replace(";", ""));
+    matchedMethods?.forEach((method) => {
+      (this as any)[method]();
+    });
+  }
+
+  public setupConfigurationForCurrentOperation() {
+    const operationName = this.crud.getCurrentOperation();
+    const setupClassName = "setup" + Str.studly(operationName) + "Operation";
+
+    this.crud.applyConfigurationFromSettings(operationName);
+
+    // run corresponding setupXxxOperation if it exists.
+    if (get_class_methods(this).includes(setupClassName)) {
+      (this as any)[setupClassName]();
+    }
+  }
+
+  /**
+   * set operation name via middleware
+   */
+  protected setOperationMiddleware(operationName: string): Middleware {
+    return {
+      handle: async (req, next) => {
+        // bind operation name to request instance
+        // this will resolved on crud.getCurrentOperation()
+        req.operation = operationName;
+        return next(req);
+      },
+    };
   }
 }
 
