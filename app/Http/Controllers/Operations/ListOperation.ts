7a0068c2e5de7c439861e56ca546d2529154f4d8
Akhmad Salafudin <axmad386@gmail.com>
2022-06-17 01:16:36 +0700

    feat(operation): added ListOperation trait

--- app/Http/Controllers/Operations/ListOperation.ts
+++ app/Http/Controllers/Operations/ListOperation.ts
@@ -2,25 +2,43 @@ import { Route, Trait } from "lunox";
 import type CrudController from "app/Http/Controllers/CrudController";
 
 export interface ListOperation {
-  setupListRoutes(segment: string, controller: typeof CrudController): void;
   index(): void;
   search(): void;
   showDetail(): void;
 }
 const ListOperation: Trait<typeof CrudController> = (s) =>
   class extends s {
-    public setupListRoutes(segment: string, controller: typeof CrudController) {
-      Route.get(segment, [controller, "index"]);
-      Route.post(segment + "/search", [controller, "search"]);
-      Route.get(segment + "/:id", [controller, "showDetail"]);
+    protected setupListRoutes(
+      segment: string,
+      controller: typeof CrudController
+    ) {
+      Route.get(segment, [controller, "index"]).middleware(
+        this.setOperationMiddleware("list")
+      );
+      Route.post(segment + "/search", [controller, "search"]).middleware(
+        this.setOperationMiddleware("list")
+      );
+      Route.get(segment + "/:id", [controller, "showDetail"]).middleware(
+        this.setOperationMiddleware("list")
+      );
+    }
+
+    /**
+     * Add the default settings, buttons, etc that this operation needs.
+     */
+    protected setupListDefaults() {
+      this.crud.configureOperation("list", () => {
+        this.crud.loadDefaultOperationSettingsFromConfig();
+      });
+      //  TODO: complete me
     }
 
     public index() {
       this.data.crud = this.crud;
 
-      // TODO: this.data tidak bisa dikirim via parameter view
-      // akan break di client side karena data CrudPanel hanya bisa diakses di server.
-      return view(this.crud.getListView(), { data: {} });
+      // this.data should be sent via view context
+      // so can be accessed via onServer method on view
+      return view(this.crud.getListView()).withContext(this.data);
     }
 
     public search() {
